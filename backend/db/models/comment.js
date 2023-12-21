1bf4a1ba42de7234d5a4b0cd309fb7d95bf51993
Daniel Calderon <danielcalderon1199@gmail.com>
2023-12-20 17:32:44 -0800

    Adding edit-comment functionality and fixing cascade delete bugs

--- backend/db/models/comment.js
+++ backend/db/models/comment.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            Comment.belongsTo(models.Post, { foreignKey: 'postId' })
+            Comment.belongsTo(models.Post, { foreignKey: 'postId', onDelete: 'CASCADE' })
             Comment.belongsTo(models.User, { foreignKey: 'userId' })
             Comment.hasMany(models.CommentImage, { foreignKey: 'commentId', onDelete: 'CASCADE' });
         }
--- backend/db/models/postImage.js
+++ backend/db/models/postImage.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            PostImage.belongsTo(models.Post, { foreignKey: "postId" })
+            PostImage.belongsTo(models.Post, { foreignKey: "postId", onDelete: 'CASCADE' })
         }
     }
     PostImage.init({
